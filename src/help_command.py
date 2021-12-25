60df91ce17e542f30a49d8e6744a25c1d7c6585a
m4ch373 <spiner47@gmail.com>
2021-12-25 22:36:03 +0800

    semi-working help command

--- src/help_command.py
+++ src/help_command.py
@@ -1,6 +1,13 @@
+# System imports
 from typing import Mapping
+from itertools import chain
+import discord
+
+# Discord imports
 from discord.ext import commands
+from discord import Embed
 from discord.ext.commands.help import HelpCommand
+from discord.ext.commands.bot import Bot
 
 class customHelpCmd(commands.HelpCommand):
 
@@ -10,5 +17,23 @@ class customHelpCmd(commands.HelpCommand):
 
     # Triggered if user only type "help"
     async def send_bot_help(self, mapping: Mapping):
-        cmd_list = '\n'.join(['\n'.join(cmd.name for cmd in mapping[cogs]) for cogs in mapping])
-        await self.get_destination().send(f"{cmd_list}")
+        # Get a list of the commands
+        cmd_list = list(chain.from_iterable(mapping[cogs] for cogs in mapping))
+
+        embed_msg = Embed(
+            title = "Command List",
+            description = "List of commands, type help [command] for more info",
+            colour = 0x9b59b6
+        )
+
+        embed_msg.set_author(
+            name = self.context.bot.user.name,
+            icon_url = self.context.bot.user.avatar_url
+        )
+
+        for cmd in cmd_list: 
+            if cmd.name != 'help':
+                value_str = f"Usage: {cmd.help}"
+                embed_msg.add_field(name = cmd.name, value = value_str, inline = False)
+
+        await self.get_destination().send(embed = embed_msg)
