f115cd5ad1dd7bb14c22ef0b21a948b39ca60bc3
m4ch373 <spiner47@gmail.com>
2022-01-01 22:11:10 +0800

    minor style adjustments

--- src/bot_commands_api.py
+++ src/bot_commands_api.py
@@ -105,37 +105,41 @@ class commandsAPI(commands.Cog, name = "API"):
             "e.g. 0 is the first result, 1 is the second, etc...")
     )
     async def anime(self, ctx: Context, *, args: str, offset: int = 0):
-        args.replace(" ", "%20")
+        async with ctx.typing():
+            args.replace(" ", "%20")
 
-        get_url = f"https://kitsu.io/api/edge/anime?filter[text]={args}&page[limit]=1&page[offset]={offset}"
-        data = requests.get(get_url).json()['data'][0]
-        print(json.dumps(data, indent = 4))
+            get_url = f"https://kitsu.io/api/edge/anime?filter[text]={args}&page[limit]=1&page[offset]={offset}"
+            data = requests.get(get_url).json()['data'][0]
+            print(json.dumps(data, indent = 4))
 
-        # Attributes
-        attr = data['attributes']
-        embed_msg = self.spawn_embed(ctx, title = f"{attr['titles']['en_jp']} | {attr['titles']['ja_jp']}")
-        embed_msg.add_field(name = "Release Date", value = f"> {attr['startDate']}")
-        embed_msg.add_field(name = "End Date", value = f"> {attr['endDate']}")
-        embed_msg.add_field(name = "Status", value = f"> {attr['status']}")
-        embed_msg.add_field(name = "Episodes", value = f"> {attr['episodeCount']}")
-        embed_msg.add_field(name = "Runtime", value = f"> {attr['episodeLength']}")
-        embed_msg.add_field(name = "Show Type", value = f"> {attr['showType']}")
+            # Attributes
+            attr = data['attributes']
+            embed_msg = self.spawn_embed(ctx, title = f"{attr['titles']['en_jp']} | {attr['titles']['ja_jp']}")
+            embed_msg.add_field(name = "Release Date", value = f"> {attr['startDate']}")
+            embed_msg.add_field(name = "End Date", value = f"> {attr['endDate']}")
+            embed_msg.add_field(name = "Status", value = f"> {attr['status']}")
+            embed_msg.add_field(name = "Episodes", value = f"> {attr['episodeCount']}")
+            embed_msg.add_field(name = "Runtime", value = f"> {attr['episodeLength']}")
+            embed_msg.add_field(name = "Show Type", value = f"> {attr['showType']}")
 
-        # Genres and Ratings and nsfw
-        genre_list = self.get_genre(data)
-        embed_msg.add_field(name = "Genre", value = f"> {', '.join(genre_list)}")
-        embed_msg.add_field(name = "NSFW", value = f"> {attr['nsfw']}")
-        embed_msg.add_field(name = "Ratings", value = f"> {attr['averageRating']}")
+            # Genres and Ratings and nsfw
+            genre_list = self.get_genre(data)
+            embed_msg.add_field(name = "Genre", value = f"> {', '.join(genre_list)}")
+            embed_msg.add_field(name = "NSFW", value = f"> {attr['nsfw']}")
+            embed_msg.add_field(name = "Ratings", value = f"> {attr['averageRating']}")
 
-        # Description
-        embed_msg.add_field(name = "Intro", value = f"{attr['description'][:300]}...")
-        embed_msg.set_image(url = attr['posterImage']['medium'])
+            # Description
+            embed_msg.add_field(name = "Intro", value = f"{attr['description'][:300]}...")
+            # embed_msg.set_image(url = attr['posterImage']['medium'])
+            embed_msg.set_thumbnail(url = attr['posterImage']['medium'])
+            embed_msg.set_image(url = attr['coverImage']['original'])
 
-        # Footer
-        embed_msg.set_footer(
-            text = (f"Not the result you expected? Type {ctx.prefix}help anime"),
-            icon_url = ctx.bot.user.avatar_url
-        )
+            # Footer
+            embed_msg.set_footer(
+                text = (f"Not the result you expected? Type {ctx.prefix}help anime"),
+                icon_url = ctx.bot.user.avatar_url
+            )
+
         await ctx.send(embed = embed_msg)
 
     def get_genre(self, data):
