5e7e09c3387f29158fba974f49e31561f20df16f
m4ch373 <spiner47@gmail.com>
2022-01-10 22:09:57 +0800

    .sesh code style update

--- src/bot_commands_common.py
+++ src/bot_commands_common.py
@@ -6,12 +6,14 @@
 from os import link, name
 import requests
 import json
+
 # Imports from discord
 import discord as discord
 from discord.ext import commands
 from discord.ext.commands import Context
 from discord.ext.commands.bot import Bot
 from discord.embeds import Embed
+
 # Additional imports
 from youtubesearchpython.__future__ import VideosSearch
 from datetime import timedelta
@@ -59,28 +61,7 @@ class commandsCommon(commands.Cog, name = "Common commands"):
     )
     async def echo(self, ctx: Context, *, args):
         await ctx.send(args)
-
-    # Error handler
-    @echo.error
-    async def echo_error(self, ctx: Context, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("No additional arguments found. Run '.help [cmd]' for further info")
     # ========================================
-
-
-    # ========================================
-    # Random command
-    # usage: rand
-    # Selects a random [common] command (not inclu. "rand")
-    @commands.command(
-        name = "rand",
-        help = "rand",
-        description = "Selects a random [common] cmd that doesn't require an arg"
-    )
-    async def rand(self, ctx: Context):
-        await ctx.send(None)
-    # ========================================
-
 
     # ========================================
     # YouTube search & return link command
@@ -113,65 +94,75 @@ class commandsCommon(commands.Cog, name = "Common commands"):
     )
     async def sesh(self, ctx: Context, user: discord.Member=None):
         # Defaults 'user' to author, if no arg is entered
-        if user == None: 
-            user = ctx.author
-        elif user.bot:
+        user = ctx.author if user == None else user
+
+        # Show error message if its a bot
+        if user.bot:
             return await ctx.send(f"Sorry, `{user.name}` is a bot 🤖")
-        # Checks if the user has an activity at the moment
-        if user.activities:
-            for activity in user.activities:
-                # EDGE CASE: Skip custom status'
-                if str(activity.type) == 'ActivityType.custom':
-                    # EDGE CASE: When custom status is the user's only activity
-                    if len(user.activities) == 1:
-                        return await ctx.send(f"❌ **`{user.name}` is not in a session right now**")
-                    continue
-                # Activity is Spotify (.listening)
-                elif isinstance(activity, discord.Spotify):
-                    embed_msg = self.spawn_embed(ctx, title = f"🎧 `{user.name}` is listening to:")
-                    # 🎶 🎧
-                    # ========================================
-                    # LEGACY
-                    # ========================================
-                    # embed_msg.description = (
-                    #     f"Title: `{activity.title}`\n"
-                    #     f"Artist(s): `{activity.artist}`\n"
-                    #     f"Album: `{activity.album}`\n"
-                    #     f"Duration: `{final_dur}`\n"
-                    #     f"**on {activity.name}**"
-                    # )
-                    # ========================================
-                    embed_msg.add_field(name = 'Name', value = f"> {activity.title}")
-                    embed_msg.add_field(name = 'Artist(s)', value = f"> {activity.artist}") 
-                    embed_msg.add_field(name = 'Album', value = f"> {activity.album}")
-                    # Simplify {duration} into h:mm:ss
-                    duration = str(activity.duration)
-                    final_dur = duration[0:7]
-                    embed_msg.add_field(name = 'Duration', value = f"> {final_dur}")
-                    
-                    embed_msg.set_thumbnail(url = activity.album_cover_url)
-                    spotify_icon_url = "https://www.freepnglogos.com/uploads/spotify-logo-png/file-spotify-logo-png-4.png"
-                    embed_msg.set_footer(text = f"on {activity.name}", icon_url = spotify_icon_url)
-                    # Set to Spotify's colour
-                    embed_msg.colour = activity.color
 
-                    await ctx.send(embed = embed_msg)
-                # Activity is a Game (.playing)
-                elif isinstance(activity, discord.Game) or isinstance(activity.type, type(discord.ActivityType.playing)):
-                    embed_msg = self.spawn_embed(ctx, title = f"🎮 `{user.name}` is playing:")
-                    embed_msg.add_field(name = 'Name', value = f"> {activity.name}", inline = False)
-                    # Convert UTC time into local time (AEDT) i.e +11hrs
-                    if activity.start is not None:
-                        aedt_start = activity.start + timedelta(hours = 11)
-                        start_str = str(aedt_start)
-                        simp_start = start_str[10:19]
-                        embed_msg.add_field(name = 'Started', value = f"> {simp_start} (AEDT)", inline = False) 
+        # Remove custom activities
+        activity_list = ([act for act in user.activities 
+            if str(act.type) != 'ActivityType.custom'])
 
-                    await ctx.send(embed = embed_msg)
-            return
         # When user.activities is niche
-        return await ctx.send(f"❌ **`{user.name}` is not in a session right now**")
+        if len(activity_list) == 0:
+            return await ctx.send(f"❌ **`{user.name}` is not in a session right now**")
+        
+        for activity in activity_list:
+            # Activity is Spotify (.listening)
+            if isinstance(activity, discord.Spotify):
+                embed_msg = self.get_spotify_embed(ctx, user, activity)
 
+            # Activity is a Game (.playing)
+            elif isinstance(activity, discord.Game) or isinstance(activity.type, type(discord.ActivityType.playing)):
+                embed_msg = self.get_game_embed(ctx, user, activity)
+
+            # Sends embed msg
+            await ctx.send(embed = embed_msg)
+
+    # Helper function
+    def get_spotify_embed(self, ctx: Context, user: discord.Member , activity: discord.Spotify):
+        embed_msg = self.spawn_embed(ctx, title = f"🎧 `{user.name}` is listening to:")
+        # 🎶 🎧
+        # ========================================
+        # LEGACY
+        # ========================================
+        # embed_msg.description = (
+        #     f"Title: `{activity.title}`\n"
+        #     f"Artist(s): `{activity.artist}`\n"
+        #     f"Album: `{activity.album}`\n"
+        #     f"Duration: `{final_dur}`\n"
+        #     f"**on {activity.name}**"
+        # )
+        # ========================================
+        embed_msg.add_field(name = 'Name', value = f"> {activity.title}")
+        embed_msg.add_field(name = 'Artist(s)', value = f"> {activity.artist}") 
+        embed_msg.add_field(name = 'Album', value = f"> {activity.album}")
+        # Simplify {duration} into h:mm:ss
+        duration = str(activity.duration)
+        final_dur = duration[0:7]
+        embed_msg.add_field(name = 'Duration', value = f"> {final_dur}")
+        
+        embed_msg.set_thumbnail(url = activity.album_cover_url)
+        spotify_icon_url = "https://www.freepnglogos.com/uploads/spotify-logo-png/file-spotify-logo-png-4.png"
+        embed_msg.set_footer(text = f"on {activity.name}", icon_url = spotify_icon_url)
+        # Set to Spotify's colour
+        embed_msg.colour = activity.color
+
+        return embed_msg
+
+    def get_game_embed(self, ctx: Context, user: discord.Member, activity):
+        embed_msg = self.spawn_embed(ctx, title = f"🎮 `{user.name}` is playing:")
+        embed_msg.add_field(name = 'Name', value = f"> {activity.name}", inline = False)
+        # Convert UTC time into local time (AEDT) i.e +11hrs
+        if activity.start is not None:
+            aedt_start = activity.start + timedelta(hours = 11)
+            start_str = str(aedt_start)
+            simp_start = start_str[10:19]
+            embed_msg.add_field(name = 'Started', value = f"> {simp_start} (AEDT)", inline = False) 
+
+        return embed_msg
+    # ========================================
 
     # ========================================
     # General Helper functinos
